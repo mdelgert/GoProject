2222d24ea7635c30c7318effec2755d0681d1205
Matthew David Elgert <mdelgert@yahoo.com>
2022-11-14 16:26:11 -0500

    Add test.go

--- Hello.go
+++ Hello.go
@@ -2,37 +2,16 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
-	//test1()
+	//fmt.Println("Hello world")
+	test1()
+	test2()
+	token := getToken()
+	fmt.Println(token)
 }
 
 func test1() {
-	url := "http://localhost:7262/api/FunctionNoAuth"
-	method := "GET"
-
-	client := &http.Client{}
-	req, err := http.NewRequest(method, url, nil)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(string(body))
+	fmt.Println("Test1")
 }
---
+++ Test.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func test2() {
+	fmt.Println("Test2")
+}
+
+func getToken() string {
+	return "Token"
+}
